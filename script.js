80b92919fba6392fad02a78b281cb7fff50f4558
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-04-29 14:30:55 +0200

    Add basic HTML and CSS and a function to render books

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    author: 'JK',
+    title: 'HP',
+    pages: 900,
+    read: true,
+  },
+  {
+    author: 'Tolkien',
+    title: 'Rings',
+    pages: 700,
+    read: false,
+  },
+];
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -7,11 +20,30 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  const book = new Book('Coelho', 'Alchimista', 300, false);
-  myLibrary.push(book);
+const bookContainer = document.querySelector('#book-container');
+
+function renderCard() {
+  myLibrary.forEach((elem) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const author = document.createElement('div');
+    author.classList.add('book-author');
+    author.textContent = elem.author;
+    card.appendChild(author);
+    const title = document.createElement('div');
+    title.classList.add('book-title');
+    title.textContent = elem.title;
+    card.appendChild(title);
+    const pages = document.createElement('div');
+    pages.classList.add('book-pages');
+    pages.textContent = elem.pages;
+    card.appendChild(pages);
+    const read = document.createElement('div');
+    read.classList.add('book-read');
+    read.textContent = elem.read;
+    card.appendChild(read);
+    bookContainer.appendChild(card);
+  });
 }
 
-addBookToLibrary();
-
-console.log(myLibrary);
+renderCard();

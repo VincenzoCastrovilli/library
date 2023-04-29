cbdb0b1ea58874dfb0876c94779ef6289fca2a5e
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-04-29 18:59:49 +0200

    Add button that calls a form to add books via user input

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const myLibrary = [
-  {
-    author: 'JK',
-    title: 'HP',
-    pages: 900,
-    read: true,
-  },
-  {
-    author: 'Tolkien',
-    title: 'Rings',
-    pages: 700,
-    read: false,
-  },
-];
+const myLibrary = [];
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -23,9 +10,10 @@ function Book(author, title, pages, read) {
 const bookContainer = document.querySelector('#book-container');
 
 function renderCard() {
-  myLibrary.forEach((elem) => {
+  myLibrary.forEach((elem, i) => {
     const card = document.createElement('div');
     card.classList.add('card');
+    card.setAttribute('data-index', i);
     const author = document.createElement('div');
     author.classList.add('book-author');
     author.textContent = elem.author;
@@ -46,4 +34,34 @@ function renderCard() {
   });
 }
 
-renderCard();
+const modal = document.querySelector('#modal');
+const addButton = document.querySelector('#add-book');
+const closeButton = document.querySelector('.close');
+
+addButton.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const author = document.getElementById('author').value;
+  const title = document.getElementById('title').value;
+  const pages = document.getElementById('pages').value;
+  const status = document.querySelector('input[type="radio"]:checked').value;
+
+  const book = new Book(author, title, pages, status);
+
+  myLibrary.push(book);
+
+  bookContainer.innerHTML = '';
+
+  renderCard();
+}
+
+const modalBtn = document.querySelector('#modal-btn');
+
+modalBtn.addEventListener('click', addBookToLibrary);
